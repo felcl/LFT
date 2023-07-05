86bec1c45b2f04b7beed00d1ebe728ef858ee177
lichenglong <2970174582@qq.com>
2023-07-05 20:37:58 +0800

    merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
+import { Modal,notification} from 'antd';
 import { useDispatch ,useSelector} from "react-redux";
 import { useWeb3React } from "@web3-react/core";
 import { useSearchParams} from "react-router-dom";
+import { useTranslation } from 'react-i18next'
 import {contract,contractInit,useConnectWallet} from './web3'
 import Axios from './axios'
 import Router from './Router/Router'
 import Header from './components/Header'
+import CloseIcon from './assets/image/CloseIcon.png'
 import './App.css'
 
 function App() {
   useConnectWallet(true)
+  const { t } = useTranslation()
+  const [,setIsBind] = useState(-1)
   const web3React = useWeb3React();
   const [search] = useSearchParams();
+  const [isinvitationModal, setIsinvitationModal] = useState(true);
+  const [invitationAddr,setInvitationAddr] = useState('')
   // const { chain, chains } = useNetwork()
   const Token = useSelector(Store =>Store.token)
   const StoreAddress = useSelector(Store =>Store.address)
@@ -39,6 +46,18 @@ function App() {
         console.log(res,"用户登录")
       })
     }
+    if(Token){
+      Axios.get('/uUser/checkBind').then(res=>{
+        if(res.data.data === 0 && res.data.code === 200){
+          setIsBind(true)
+        }
+        if(res.data.data === 1 && res.data.code === 200){
+          setIsBind(false)
+        }
+          console.log(res,'用户是否绑定上级')
+      })
+    }
+
   },[web3React.active,web3React.account,Token,StoreAddress])
   useEffect(()=>{
     if(web3React.active && Object.keys(contract).length <=0){
@@ -46,12 +65,49 @@ function App() {
       contractInit()
     }
   },[web3React.active])
+  const changeInvitationAddr = (e) => {
+      setInvitationAddr(e.target.value)
+      // console.log(e.target.value)
+  }
+  const invitationAddrFun = () => {
+    if(!invitationAddr){
+        return notification.open({
+            message: 'Error',
+            description:t('Pleaseenterthe3')
+        });
+    }
+    Axios.post('/uUser/bind',{
+        refereeUserAddress:invitationAddr
+    }).then(res=>{
+      if(res.data.code === 200){
+        setIsinvitationModal(false)
+        notification.open({
+            message: 'Success',
+            description:t('bindsuccessfully')
+        });
+    }else{
+        console.log(res.data.msg)
+        notification.open({
+            message: 'Success',
+            description:t('bindingfailed')
+        });
+    }
+    })
+}
   return (
     <>
     
       <Header></Header>
       <Router></Router>
     {/* <span onClick={()=>connect({ connector: connectors[1] })}> 链接</span> */}
+        {/* 邀请弹窗 */}
+    <Modal open={isinvitationModal} onCancel={()=>{setIsinvitationModal(false)}} closable={false} maskClosable={false} footer={null} wrapClassName="modalBox" width="676px">
+      <div className="Title">{t('Invitationaddress')}</div>
+      <div className='putBox'>
+          <input type="text" placeholder='Enter invitation address' onChange={changeInvitationAddr} />
+      </div>
+      <div className="Confirm flexCenter" onClick={invitationAddrFun}>{t('Confirm')}</div>
+    </Modal>
     </>
   )
 }
